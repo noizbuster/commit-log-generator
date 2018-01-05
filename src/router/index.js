a7d7b7b01cc06aa3c6b97a63901344954453ecde
NoizBuster <noizbuster@noizbuster.com>
2018-01-05 19:01:06 +0900

    added vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+Vue.use(Vuetify)
 
 export default new Router({
   routes: [
